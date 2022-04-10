6f3891d0c63ca67efb98848d7554c3e9955cda3e
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-04-10 01:53:10 -0300

    module 1 - video 07 finished

--- js/main.js
+++ js/main.js
@@ -36,10 +36,36 @@ const initNavigation = function initializeTheNavigationAnimations() {
   });
 };
 //header
+const moveImagesAnimation = function createTheMoveImagesEffect(
+  xPos,
+  yPos,
+  images
+) {
+  const modifier = (index) => index * 1.2 + 0.5;
+
+  images.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+    });
+  });
+};
+
 const moveImages = function moveTheImagesInTheHeader(e) {
   const { offsetX, offsetY, target } = e;
   const { clientWidth, clientHeight } = target;
-  console.log(offsetX, offsetY, clientWidth, clientHeight);
+
+  const xPos = offsetX / clientWidth - 0.5;
+  const yPos = offsetY / clientHeight - 0.5;
+
+  const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+  const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+  moveImagesAnimation(xPos, yPos, leftImages);
+  moveImagesAnimation(xPos, yPos, rightImages);
 };
 const initHeaderTilt = function initializeTheImgHeaderTiltAnimation() {
   document.querySelector('header').addEventListener('mousemove', moveImages);
