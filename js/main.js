62d4ec8f217251ee7c9bcb5ac190dda25d9d2870
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-04-13 00:01:46 -0300

    module 2 - video 14 doing it

--- js/main.js
+++ js/main.js
@@ -77,9 +77,12 @@ const moveImages = function moveTheImagesInTheHeader(e) {
 const initHeaderTilt = function initializeTheImgHeaderTiltAnimation() {
   document.querySelector('header').addEventListener('mousemove', moveImages);
 };
+
 //reveal gallery
+const getTextHeight = (textCopy) => textCopy.clientHeight;
+
 const createHoverReveal = function createTheHoverRevealAnimation(e) {
-  const { mask, imageBlock } = e.target;
+  const { mask, imageBlock, text, textCopy, textMask, textP, image } = e.target;
   let tl = gsap.timeline({
     defaults: {
       duration: 0.7,
@@ -88,10 +91,15 @@ const createHoverReveal = function createTheHoverRevealAnimation(e) {
   });
 
   if (e.type === 'mouseenter') {
-    tl.to([mask, imageBlock], { yPercent: 0 });
+    tl.to([mask, imageBlock, textP, textMask], { yPercent: 0 })
+      .to(text, { y: () => -getTextHeight(textCopy) / 2 }, 0)
+      .to(image, { duration: 1.1, scale: 1 }, 0);
   }
   if (e.type === 'mouseleave') {
-    tl.to(mask, { yPercent: 100 }).to(imageBlock, { yPercent: -101 }, 0);
+    tl.to([mask, textP], { yPercent: 100 })
+      .to([imageBlock, textMask], { yPercent: -101 }, 0)
+      .to(text, { y: 0 }, 0)
+      .to(image, { duration: 1.1, scale: 1.2 }, 0);
   }
   return tl;
 };
@@ -100,11 +108,17 @@ const initHoverReveal = function initializeTheGalleryRevealOnHover() {
   const sections = document.querySelectorAll('.rg__column');
   sections.forEach((section) => {
     section.imageBlock = section.querySelector('.rg__image');
+    section.image = section.querySelector('.rg__image img');
     section.mask = section.querySelector('.rg__image--mask');
+    section.text = section.querySelector('.rg__text');
+    section.textCopy = section.querySelector('.rg__text--copy');
+    section.textMask = section.querySelector('.rg__text--mask');
+    section.textP = section.querySelector('.rg__text--copy p');
 
     //reset the original position
-    gsap.set(section.imageBlock, { yPercent: -101 });
-    gsap.set(section.mask, { yPercent: 100 });
+    gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
+    gsap.set([section.mask, section.textP], { yPercent: 100 });
+    gsap.set(section.image, { scale: 1.2 });
 
     //add event listeners to each section
     section.addEventListener('mouseenter', createHoverReveal);
@@ -112,10 +126,23 @@ const initHoverReveal = function initializeTheGalleryRevealOnHover() {
   });
 };
 
+//js media queries
+const mq = window.matchMedia('(min-width: 768px');
+
+const handleWidthChange = function handleWindowWidthChange(mq) {
+  console.log(mq.matches);
+  if (mq.matches) {
+    initHoverReveal();
+  } else {
+    console.log('we are on mobile');
+  }
+};
+mq.addEventListener('change', handleWidthChange);
+
 const init = function initializeTheCode() {
   initNavigation();
   initHeaderTilt();
-  initHoverReveal();
+  handleWidthChange(mq);
 };
 
 window.addEventListener('load', () => init());
